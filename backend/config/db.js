9f747bf501cf0d4af09ff293df71e40851436a7d
“Ishan <ishanbathla69@gmail.com>
2024-10-03 12:05:51 +0530

    demo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-  await mongoose.connect('mongodb+srv://purvamadaan418:BBiwox1D26ijfvep@cluster0.r38eh.mongodb.net/biteCart').then(()=>console.log("DB connected"))
+  await mongoose.connect('mongodb+srv://ishanbathla28:fWIQCqPN1h6QM1E7@cluster0.valf0.mongodb.net/bitecart').then(()=>console.log("DB connected"))
 }
